206d9e6eb28bfbdcd2f42e024bce0248de17ce14
Tyuiu.SvitkovIA@outlook.com <svitkovia@mail.ru>
2023-11-07 23:45:07 +0500

    Task6

---
+++ Tyuiu.SvitkovIA.Sprint4.Task6.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SvitkovIA.Sprint4.Task6.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SvitkovIA.Sprint4.Task6.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("67ba4a5b-16b8-4194-881d-ca055d76ecb2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.SvitkovIA.Sprint4.Task6.V11/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.SvitkovIA.Sprint4.Task6.V11.Lib;
+
+namespace Tyuiu.SvitkovIA.Sprint4.Task6.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Свитков И. А. | АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Класс Array                                                       *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант 11                                                              *");
+            Console.WriteLine("* Выполнил: Свитков Иван Андреевич | АСОиУб-23-2                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан строковый массив данных,используя классArray подсчитайте количество *");
+            Console.WriteLine("* элементов, длина которых равна 5.                                       *");
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            var animal = new string[] { "Кошка", "Собака", "Слон", "Жираф", "Бегемот", "Игуана", "Ягуар" };
+
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i <= animal.Length - 1; i++)
+            {
+                Console.WriteLine(animal[i]);
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Количество элементов длина которых = 5:");
+
+            int nums = ds.Calculate(animal);
+
+            Console.WriteLine(nums);
+            Console.ReadKey();
+        }
+    }
+}
